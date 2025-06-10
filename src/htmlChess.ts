c313e605e1dc2b5d52986727736492beb9d276de
Voikyrioh <voikyrioh@gmail.com>
2025-06-10 22:43:33 +0200

    Fixed fen notation + added to url. added checkmate

--- src/htmlChess.ts
+++ src/htmlChess.ts
@@ -10,7 +10,7 @@ import type {Move} from "./game/entities/game/move.ts";
 const fenUrl = new URLSearchParams(document.location.search).get('fen');
 const game = new Gameplay(fenUrl ?? undefined);
 
-function getBoardChessChar(pos: string): string {
+function getBoardChessChar(pos: string): string|null {
     const piece = game.getPieceFromPosition(Position.fromString(pos));
 
     if (!piece) return '';
@@ -21,7 +21,7 @@ function getBoardChessChar(pos: string): string {
         case 'bishop': return '♝';
         case 'queen': return '♛';
         case 'king': return '♚';
-        default: return '';
+        default: return null;
     }
 }
 
@@ -42,7 +42,6 @@ for (let r = 1; r <= 8; r++) {
 function renderPossibleMoves(piece: ChessPiece) {
     defaultClick();
     game.getPossibleMoves(piece).forEach(move => {
-        console.log(move);
         const ca = cases.get(move.to.toString());
         if (ca) {
             ca.classList.add('possible-move');
@@ -53,23 +52,39 @@ function renderPossibleMoves(piece: ChessPiece) {
 
 function render() {
     cases.forEach((p, pos) => {
-        p.innerText = getBoardChessChar(pos);
-        let piece = game.getPieceFromPosition(Position.fromString(pos));
+        const piece = getBoardChessChar(pos);
         if (piece) {
-            p.classList.add('piece');
-            if (piece.color === 'white') {
-                p.classList.remove('piece-black');
-                p.classList.add('piece-white');
+            const pieceElement = document.createElement('div');
+            pieceElement.textContent = piece;
+            let gamePiece = game.getPieceFromPosition(Position.fromString(pos));
+            if (gamePiece) {
+                pieceElement.classList.add('piece');
+                if (gamePiece.color === 'white') {
+                    pieceElement.classList.remove('piece-black');
+                    pieceElement.classList.add('piece-white');
+                } else {
+                    pieceElement.classList.remove('piece-white');
+                    pieceElement.classList.add('piece-black');
+                }
+                pieceElement.draggable = true;
+                pieceElement.ondragstart = () => {renderPossibleMoves(gamePiece)}
             } else {
-                p.classList.remove('piece-white');
-                p.classList.add('piece-black');
+                pieceElement.classList.remove('piece');
+                pieceElement.draggable = false;
+                pieceElement.ondragstart = () => defaultClick();
             }
-            p.onclick = () => renderPossibleMoves(piece)
+            pieceElement.ondrop = canDrop;
+            p.appendChild(pieceElement);
         } else {
-            p.classList.remove('piece');
-            p.onclick = () => defaultClick();
+            p.innerHTML = '';
+            p.draggable = false;
+            p.ondragstart = () => defaultClick();
         }
     })
+}
+
+function canDrop(e: DragEvent) {
+    console.log("event:", e);
 }
 
 function defaultClick() {
